bc1e93801708c02a43482f4a873df598bb7e34b7
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-10-12 21:23:53 +0000

    more chnages

--- assets/script.js
+++ assets/script.js
@@ -67,33 +67,33 @@ document.getElementById("check").onclick = () => {
 // });
 
 
-var locationDisplay = getElementById("location-coordinates");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    document.getElementById("location-coordinates").setAttribute('value', "Geoloacation is not supported by this browser");
-  }
-}
+// var locationDisplay = getElementById("location-coordinates");
+// function getLocation() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(showPosition);
+//   } else {
+//     document.getElementById("location-coordinates").setAttribute('value', "Geoloacation is not supported by this browser");
+//   }
+// }
 
 
-function showPosition(position) {
+// function showPosition(position) {
   
-  document.getElementById("location-coordinates").setAttribute('value', "Latitude: " + position.coords.latitude +
-  "Longitude: " + position.coords.longitude);
+//   document.getElementById("location-coordinates").setAttribute('value', "Latitude: " + position.coords.latitude +
+//   "Longitude: " + position.coords.longitude);
   
-}
+// }
 
 
-function dateStamp() {
-  document.getElementById('todays-date').valueAsDate = new Date();
-}
+// function dateStamp() {
+//   document.getElementById('todays-date').valueAsDate = new Date();
+// }
 
-function timeStamp() {
-const now = new Date();
-const current = now.getHours() + ":" + now.getMinutes();
-document.getElementById("time-stamp").value = current;
-}
+// function timeStamp() {
+// const now = new Date();
+// const current = now.getHours() + ":" + now.getMinutes();
+// document.getElementById("time-stamp").value = current;
+// }
 
 document.getElementById("read").addEventListener("click", async () => {
   log("readLog", "User clicked scan button");
@@ -115,8 +115,6 @@ document.getElementById("read").addEventListener("click", async () => {
       if (serialNumber = "c2:5f:2d:5e") {
         log("readLog", "Fuck Yes string !!!!")
         document.getElementById("serial-number").setAttribute('value', serialNumber);
-        
-        
       } else {
         log("readLog", "Didn't work")
         document.getElementById("serial-number").setAttribute('value', serialNumber);
