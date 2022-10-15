e9f41207f675429edad854c4883bb01625fa411d
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-10-15 14:04:41 +0000

    test 2

--- assets/script.js
+++ assets/script.js
@@ -20,37 +20,37 @@ document.getElementById("check").onclick = () => {
   }, 3000);
 };
 
+// my nfc code------------------------------------------------------------------------------------------*
+// document.getElementById("read").addEventListener("click", async () => {
+//   log("readLog", "User clicked scan button");
 
-document.getElementById("read").addEventListener("click", async () => {
-  log("readLog", "User clicked scan button");
+//   try {
+//     const ndef = new NDEFReader();
+//     await ndef.scan();
+//     log("readLog", "> Scan started");
 
-  try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    log("readLog", "> Scan started");
+//     ndef.addEventListener("readingerror", () => {
+//       log(
+//         "readLog",
+//         "Argh! Cannot read data from the NFC tag. Try another one?"
+//       );
+//     });
 
-    ndef.addEventListener("readingerror", () => {
-      log(
-        "readLog",
-        "Argh! Cannot read data from the NFC tag. Try another one?"
-      );
-    });
-
-    ndef.addEventListener("reading", ({serialNumber }) => {
-      log("readLog", `> Go on then: ${serialNumber}`);
-      if ({serialNumber }) {
-        log("readLog", "time stamp Yes string !!!!")
-        document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
-      } else {
-        log("readLog", "Didn't work")
-        document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
-      };
+//     ndef.addEventListener("reading", ({serialNumber }) => {
+//       log("readLog", `> Go on then: ${serialNumber}`);
+//       if ({serialNumber }) {
+//         log("readLog", "time stamp Yes string !!!!")
+//         document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
+//       } else {
+//         log("readLog", "Didn't work")
+//         document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
+//       };
       
-    });
-  } catch (error) {
-    log("readLog", "Argh! " + error);
-  }
-});
+//     });
+//   } catch (error) {
+//     log("readLog", "Argh! " + error);
+//   }
+// });
 
 
 //locatation code ---------------------------------------------------------------------------------------------------*
@@ -80,3 +80,58 @@ document.getElementById("read").addEventListener("click", async () => {
 // document.getElementById("time-stamp").value = current;
 // document.getElementById('todays-date').valueAsDate = now;
 // }
+
+
+// original nfc code----------------------------------------------------------------------------------------------------------*
+
+document.getElementById("read").addEventListener("click", async () => {
+  log("readLog", "User clicked scan button");
+
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    log("readLog", "> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      log(
+        "readLog",
+        "Argh! Cannot read data from the NFC tag. Try another one?"
+      );
+    });
+
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      log("readLog", `> Serial Number: ${serialNumber}`);
+      log("readLog", `> Records: (${message.records.length})`);
+
+      const decoder = new TextDecoder();
+      for (const record of message.records) {
+        switch (record.recordType) {
+          case "text":
+            const textDecoder = new TextDecoder(record.encoding);
+            log(
+              "readLog",
+              `Text: ${textDecoder.decode(record.data)} (${record.lang})`
+            );
+            break;
+          case "url":
+            log("readLog", `URL: ${decoder.decode(record.data)}`);
+            break;
+          case "mime":
+            if (record.mediaType === "application/json") {
+              log(
+                "readLog",
+                `JSON: ${JSON.parse(decoder.decode(record.data))}`
+              );
+            } else {
+              log("readLog", `Media not handled`);
+            }
+            break;
+          default:
+            log("readLog", `Record not handled`);
+        }
+      }
+    });
+  } catch (error) {
+    log("readLog", "Argh! " + error);
+  }
+});
