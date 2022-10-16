412017acd63ebedac35c433c526fa31b2fee3928
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2022-10-16 12:07:35 +0000

    .......

--- assets/script.js
+++ assets/script.js
@@ -54,22 +54,22 @@ document.getElementById("check").onclick = () => {
 
 
 //locatation code ---------------------------------------------------------------------------------------------------*
-// var locationDisplay = getElementById("location-coordinates");
+var locationDisplay = getElementById("location-coordinates");
 
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     document.getElementById("location-coordinates").setAttribute('value', "Geolocation is not supported by this browser");
-//   }
-// }
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    document.getElementById("location-coordinates").setAttribute('value', "Geolocation is not supported by this browser");
+  }
+}
 
-// function showPosition(position) {
+function showPosition(position) {
   
-//   document.getElementById("location-coordinates").setAttribute('value', "Latitude: " + position.coords.latitude +
-//   "Longitude: " + position.coords.longitude);
+  document.getElementById("location-coordinates").setAttribute('value', "Latitude: " + position.coords.latitude +
+  "Longitude: " + position.coords.longitude);
   
-// }
+}
 
 
 // time stamp code----------------------------------------------------------------------------------------------------*
@@ -105,8 +105,8 @@ document.getElementById("read").addEventListener("click", async () => {
         log("readLog", "time stamp Yes string !!!!")
         document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
         timeStamp();
-        // getLocation();
-        // showPosition(position);
+        getLocation();
+        showPosition(position);
       } else {
         log("readLog", "Didn't work")
         document.getElementById("serial-number").setAttribute('value', `${serialNumber }`);
